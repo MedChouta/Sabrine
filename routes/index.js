207ae0c6f98f1d26f504090fe99378625c58838b
mohammed chouta <mohammedchouta10@gmail.com>
2023-08-20 08:14:08 -0400

    added doc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const fs = require("fs")
-const axios = require("axios")
-const fileUpload = require("express-fileupload")
+const fs = require("fs") //file system module
+const axios = require("axios") //promise-based HTTP Client lib
+const fileUpload = require("express-fileupload") //file upload lib, used in the /transcribe route
 const Converter = require('../converter')
 const express = require('express')
-const http = require('http')
 const path = require('path')
 const router = express.Router()
 
@@ -12,24 +11,31 @@ router.use(
 );
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/../views/index.html'))
+  res.sendFile(path.join(__dirname, '/../views/index.html')) //file upload form 
 });
 
 router.post('/transcribe', (req, res, next) => {
 
-  console.log(req.files.audio)
+  var video = req.files.video //video to be processed
 
-  var audio = req.files.audio.name
+  console.log(video)
 
-  fs.writeFile(path.join(__dirname, `../tmp/${audio}`), req.files.audio.data, err => {
+
+  //Saves the video file in the tmp folder
+  fs.writeFile(path.join(__dirname, `../tmp/${video.name}`), video.data, err => {
     if (err) {
       console.error("Error Code:")
       console.error(err);
     }
   })
   
-  let convert = new Converter(path.join(__dirname, `../tmp/${audio}`))
-  
+  let convert = new Converter(path.join(__dirname, `../tmp/${video.name}`)) //initializes the constructor
+
+  /**
+   * Converts the video file to audio (mp3) with ConvertToAudio
+   * 
+   * Sends the GET request right after the completion of the convertion with parameter "audio_file" (will be used in the python server)
+   */
   convert.ConvertToAudio().then((code) => {
       
     axios
