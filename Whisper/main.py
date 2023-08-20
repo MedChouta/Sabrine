13034887dc8c3e70cf4120851bc0930021060063
mohammed chouta <mohammedchouta10@gmail.com>
2023-08-20 08:14:39 -0400

    added doc

--- Whisper/main.py
+++ Whisper/main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def transcribe():
 
-    print(request.args)
+    print(request.args) #debug msg
 
     transcript = ""
-    filename = request.args.get("audio_file")
+    filename = request.args.get("audio_file") #url argument with file name only
 
     with open("../tmp/" + filename, "r") as audio_file:
         t = Transcribe(audio_file.name)
--- Whisper/transcribe.py
+++ Whisper/transcribe.py
@@ -1,13 +1,23 @@
 from faster_whisper import WhisperModel
 
 class Transcribe(WhisperModel):
-    """
-        Inherits the WhisperModel class
 
-        This class will handle every aspect of the audio transcription 
+    """Handles every aspect of the audio transcription 
+        
+        Inherits:
+            WhisperModel
 
     """
     def __init__(self, filename, model_size="medium", device="cpu", compute_type="int8"):
+
+        """
+            Args:
+                filename (str): file location of the audio file
+                model_size (str): values accepted {tiny, base, small, medium, large-v1, large-v2}
+                divice (str): cpu or cuda (nvidia)
+                compute_type (str): floating point precision
+        """
+
         self.filename = filename
         self.model_size = model_size
         self.device = device
@@ -18,10 +28,9 @@ class Transcribe(WhisperModel):
     
     def transcribe(self, b_size=5):
 
-        """
-            returns the full transcript of the mp3 file
+        """returns the full transcript of the mp3 file
             
-            uses the original transcribe method from the WhisperModel class
+            uses the original transcribe method from the WhisperModel (faster-whisper) class (see faster-whisper github example)
         """
 
         transcript = ""
